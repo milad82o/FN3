564507d72dd89c589fbc56606c7fc80b8ccdbc4c
milad82mi-byte <milad82mi@gmail.com>
2026-05-18 17:59:08 +0330

    Update start.sh

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-sudo /usr/local/bin/xray run -c /etc/xray/g2ray.json &>/tmp/xray.log &
-sleep 2
-show-link.sh
-echo "[g2ray] Keep-alive active..."
-while true; do sleep 300; echo "[$(date '+%H:%M')] alive" >>/tmp/keepalive.log; done
+
+echo "[g2ray auto-restart] Started..."
+
+while true; do
+  sudo /usr/local/bin/xray run -c /etc/xray/g2ray.json &>/tmp/xray.log &
+  PID=$!
+
+  sleep 2
+  show-link.sh
+
+  echo "[g2ray] Running with PID $PID"
+
+  # Keepalive every 3 minutes for 3 hours
+  for ((i=0; i<60; i++)); do
+    sleep 180
+    echo "1"
+  done
+
+  echo "[g2ray] Stopping..."
+  kill $PID
+
+  sleep 5
+
+  echo "[g2ray] Restarting..."
+done
